edf349720bf5d185a9ed6df8e88442a175f9d60b
Trigun Joshi <iamtrigun@gmail.com>
2023-07-15 12:20:10 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import turtle as t
+import random
+
+tim = t.Turtle()
+t.colormode(255)
+
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    ran_color = (r, g, b)
+    return ran_color
+
+
+directions = [0, 90, 180, 270]
+
+tim.pensize(15)
+tim.speed("fastest")
+for _ in range(2000):
+    tim.color(random_color())
+    tim.forward(30)
+    tim.setheading(random.choice(directions))
